49d0e32c0957007cd6228debd90719ff3bff4ce8
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2022-09-26 13:57:23 +0800

    Added an example CLI that can be used to test out the library
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

---
+++ examples/cli.rs
@@ -0,0 +1,30 @@
+pub fn main() {
+    #[allow(unused_variables)]
+    let arg = std::env::args()
+        .skip(1)
+        .next()
+        .expect("Must provide at least one argument");
+
+    #[cfg(feature = "time")]
+    {
+        use interim::{parse_date_string, Dialect};
+        use time::OffsetDateTime;
+        println!(
+            "{}",
+            parse_date_string(arg.as_str(), OffsetDateTime::now_utc(), Dialect::Us).unwrap()
+        );
+    }
+    #[cfg(feature = "chrono")]
+    {
+        use chrono::Local;
+        use interim::{parse_date_string, Dialect};
+        println!(
+            "{}",
+            parse_date_string(arg.as_str(), Local::now(), Dialect::Us).unwrap()
+        );
+    }
+    #[cfg(not(any(feature = "time", feature = "chrono")))]
+    {
+        eprintln!("Please enable either time or chrono feature")
+    }
+}
